4d08376d68b757a70209b0d5c8b77c10197a43ed
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-09-25 13:59:45 +0200

    fix(hotfix for null values on createlinks): fixes a crash whenn createLinks was passed a null value
    Needs more automated tests

--- sources/packages/flusshygiene-cms-spa/src/lib/utils/questionnaire-additional-texts-filter.ts
+++ sources/packages/flusshygiene-cms-spa/src/lib/utils/questionnaire-additional-texts-filter.ts
@@ -1,4 +1,7 @@
-export const createLinks: (text: string) => string = (text) => {
+export const createLinks: (text: string | null) => string = (text) => {
+  if (text === null) {
+    return '';
+  }
   const res = text.replace(
     /<(.*?)>/g,
     '<a href="$1" target="_blank" rel="noopener noreferrer"><em>[Link]</em></a>',
