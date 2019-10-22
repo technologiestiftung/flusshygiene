a4409736023821e7327880ed4f6cbcb969f60b1c
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-10-22 17:35:26 +0200

    add data

--- sources/packages/flusshygiene-cms-spa/src/questionnaire-data/index.ts
+++ sources/packages/flusshygiene-cms-spa/src/questionnaire-data/index.ts
@@ -1,5 +1,4 @@
 const filenames = [
-  'Frage-10.3.1.-gew2.json',
   'Frage-1.1.1.-gew2.json',
   'Frage-2.1.1.-gew4.json',
   'Frage-2.2.1.-gew4.json',
@@ -7,6 +6,11 @@ const filenames = [
   'Frage-2.4.1.-gew4.json',
   'Frage-3.1.1.-gew1.json',
   'Frage-3.2.1.-gew0.json',
+  'Frage-3.2.2.-gew2.json',
+  'Frage-3.2.3.-gew4.json',
+  'Frage-3.3.1.-gew0.json',
+  'Frage-3.3.2.-gew3.json',
+  'Frage-3.3.3.-gew3.json',
   'Frage-4.1.1.-gew3.json',
   'Frage-4.2.1.-gew3.json',
   'Frage-4.3.1.-gew3.json',
@@ -22,33 +26,8 @@ const filenames = [
   'Frage-9.2.1.-gew3.json',
   'Frage-10.1.1.-gew2.json',
   'Frage-10.2.1.-gew2.json',
+  'Frage-10.3.1.-gew2.json',
 ];
-
-// [
-//   'Frage-1.1.1.-gew2.json',
-//   'Frage-2.1.1.-gew4.json',
-//   'Frage-2.2.1.-gew4.json',
-//   'Frage-2.3.1.-gew4.json',
-//   'Frage-2.4.1.-gew4.json',
-//   'Frage-3.1.1.-gew1.json',
-//   'Frage-3.2.1.-gew0.json',
-//   'Frage-4.1.1.-gew3.json',
-//   'Frage-4.2.1.-gew3.json',
-//   'Frage-4.3.1.-gew3.json',
-//   'Frage-4.4.1.-gew3.json',
-//   'Frage-5.1.1.-gew3.json',
-//   'Frage-6.1.1.-gew2.json',
-//   'Frage-7.1.1.-gew4.json',
-//   'Frage-8.1.1.-gew4.json',
-//   'Frage-8.2.1.-gew4.json',
-//   'Frage-8.3.1.-gew4.json',
-//   'Frage-8.4.1.-gew4.json',
-//   'Frage-9.1.1.-gew3.json',
-//   'Frage-9.2.1.-gew3.json',
-//   'Frage-10.1.1.-gew2.json',
-//   'Frage-10.2.1.-gew2.json',
-//   'Frage-10.3.1.-gew2.json',
-// ];
 
 export const getQuestions = async () => {
   try {
