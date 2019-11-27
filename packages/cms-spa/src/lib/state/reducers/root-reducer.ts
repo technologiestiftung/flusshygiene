2216877297ae12c574386b366023f284a9bbf559
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-11-27 20:15:15 +0100

    chore(housekeeping)

--- packages/cms-spa/src/lib/state/reducers/root-reducer.ts
+++ packages/cms-spa/src/lib/state/reducers/root-reducer.ts
@@ -1,15 +1,11 @@
 import { combineReducers } from 'redux';
-import ui from './ui-reducer';
 import data from './spots-reducer';
 import detailSpot from './single-spot-reducer';
 import postSpot from './spot-post-reducer';
-import questionaire from './questionnaire-reducer';
 const rootReducer = combineReducers({
-  ui,
   data,
   detailSpot,
   postSpot,
-  questionaire,
 });
 export type RootState = ReturnType<typeof rootReducer>;
 export default rootReducer;
