a46c29240ff073f7df503480b6be39f4e045bf34
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-11-30 13:14:44 +0100

    chore(node and test setup): foo

--- packages/cms-spa/src/setupTests.ts
+++ packages/cms-spa/src/setupTests.ts
@@ -1,5 +1,9 @@
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom/extend-expect';
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
 // import '@testing-library/react/cleanup-after-each';
 
 // console.info('globel setup done');
