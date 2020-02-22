75ea9d7ce60a8939da2aa2d62c9b9e0cb92e5be9
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-02-22 20:40:57 +0100

    feature(install.r): Adds bash script to create R script with PAT and version

---
+++ packages/opencpu-fhpredict-api/build-r-installer.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+# Creates a R file that we can use to install or packages
+# expects two arguments:
+# $1 is the Personal Access Token
+# $2 is the version of fhpredict we are targeting
+if [ $# -lt 2 ]; then
+	printf "Not enough arguments"
+  printf "Expects GitHub PAT as first argument"
+  printf "and version of fhpredictas second argument"
+
+	exit 1
+
+else
+
+  cat << EOF
+  #!/usr/bin/env Rscript
+  Sys.setenv(GITHUB_PAT = "${1}")
+  remotes::install_github("kwb-r/fhpredict@${2}", build_vignettes = FALSE, force = TRUE, upgrade = "never")
+EOF
+
+fi
+
+
+
