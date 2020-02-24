f61d64c4e1d49c0d92c0d2f6ffbd14da1f3ebc6d
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-02-24 12:20:55 +0100

    fix([error]src/__mocks__/winston.ts(44,1): error TS1005: '}' expected.)

--- packages/opencpu-middlelayer/src/__mocks__/winston.ts
+++ packages/opencpu-middlelayer/src/__mocks__/winston.ts
@@ -2,7 +2,7 @@ const logger = {
   debug: jest.fn(),
   log: jest.fn(),
   info: jest.fn(),
-  error: jest.fn()
+  error: jest.fn(),
 };
 
 // if you want this only in one file
@@ -24,20 +24,19 @@ const logger = {
 // // }));
 
 // // IMPORTANT First mock winston
-module.exports =  {
-    format: {
-      colorize: jest.fn(),
-      combine: jest.fn(),
-      label: jest.fn(),
-      timestamp: jest.fn(),
-      printf: jest.fn(),
-      metadata: jest.fn(),
-      json: jest.fn(),
-    },
-    createLogger: jest.fn().mockReturnValue(logger),
-    transports: {
-      Console: jest.fn(),
-      File: jest.fn(),
-    },
-
-
+module.exports = {
+  format: {
+    colorize: jest.fn(),
+    combine: jest.fn(),
+    label: jest.fn(),
+    timestamp: jest.fn(),
+    printf: jest.fn(),
+    metadata: jest.fn(),
+    json: jest.fn(),
+  },
+  createLogger: jest.fn().mockReturnValue(logger),
+  transports: {
+    Console: jest.fn(),
+    File: jest.fn(),
+  },
+};
