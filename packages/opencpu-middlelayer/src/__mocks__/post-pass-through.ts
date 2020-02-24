db787840ed3382ad3e155f220093362dba626933
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-02-24 12:23:24 +0100

    fix([error]src/__mocks__/post-pass-through.ts(4,3): error TS6133: 'url' is declared but its value is never read.)

--- packages/opencpu-middlelayer/src/__mocks__/post-pass-through.ts
+++ packages/opencpu-middlelayer/src/__mocks__/post-pass-through.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { IObject } from '../common/interfaces';
 
 module.exports = async function postPassThrough(
-  url: string,
+  _url: string,
   body: IObject,
 ): Promise<IObject> {
   return await new Promise((resolve) => {
