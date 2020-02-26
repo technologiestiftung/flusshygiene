92174e39b13e0dab69e6c7922ba1b68cbbeaf036
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-02-26 10:12:54 +0100

    chore(remove old code)

--- packages/cronbot/src/lib/report.ts
+++ packages/cronbot/src/lib/report.ts
@@ -1,11 +1,8 @@
 import { globals } from "./../common/globals";
 import fs, { ReadStream } from "fs";
-import got from "got";
-import FormData from "form-data";
-
 import { DB } from "./DB";
 import { IReport, IBuildReport } from "./../common/interfaces";
-import { MAILGUN_TO, MAILGUN_FROM } from "../common/env";
+// import { MAILGUN_TO, MAILGUN_FROM } from "../common/env";
 import { logger } from "../utils/logger";
 import { resolve } from "path";
 import nodemailer from "nodemailer";
@@ -15,28 +12,28 @@ const db = DB.getInstance();
 /**
  * @deprecated in favour of nodemailer
  */
-export const setupForm: (data: {
-  from: string;
-  to: string;
-  subject: string;
-  text: string;
-  html: string;
-  addFile: boolean;
-}) => FormData = ({ from, to, subject, text, html, addFile }) => {
-  const form = new FormData();
-  form.append("from", from);
-  form.append("to", to);
-  form.append("subject", subject);
-  form.append("text", text);
-  form.append("html", `<body><pre><code>${html}</code></pre></body>`);
-  if (addFile) {
-    form.append(
-      "attachment",
-      fs.createReadStream(resolve(process.cwd(), "db.json")),
-    );
-  }
-  return form;
-};
+// export const setupForm: (data: {
+//   from: string;
+//   to: string;
+//   subject: string;
+//   text: string;
+//   html: string;
+//   addFile: boolean;
+// }) => FormData = ({ from, to, subject, text, html, addFile }) => {
+//   const form = new FormData();
+//   form.append("from", from);
+//   form.append("to", to);
+//   form.append("subject", subject);
+//   form.append("text", text);
+//   form.append("html", `<body><pre><code>${html}</code></pre></body>`);
+//   if (addFile) {
+//     form.append(
+//       "attachment",
+//       fs.createReadStream(resolve(process.cwd(), "db.json")),
+//     );
+//   }
+//   return form;
+// };
 
 interface IBuildMessage {
   text: string;
@@ -65,7 +62,8 @@ const buildMessage: (options: IBuildMessage) => any = ({
   return { text, html, subject, from, to, attachments };
 };
 
-const wrapInCodeTags = (text: string) => `<pre><code>${text}</code></pre>`;
+export const wrapInCodeTags = (text: string) =>
+  `<pre><code>${text}</code></pre>`;
 export const sendReports: () => Promise<void> = async () => {
   const host = process.env.SMTP_HOST;
   const user = process.env.SMTP_USER;
@@ -131,26 +129,15 @@ export const sendReports: () => Promise<void> = async () => {
         return message;
       });
 
-      let done = false;
-      let count = 0;
-
       for (const message of messages) {
-        transporter.sendMail(message, (err: Error, info: any) => {
-          if (err) {
-            logger.error(err);
-            count++;
-          } else {
-            count++;
-            logger.info(info);
-          }
-          if (count === messages.length) {
-            done = true;
-          }
-          if (done === true) {
-            transporter.close();
-          }
-        });
+        try {
+          const info = await transporter.sendMail(message);
+          logger.info(info);
+        } catch (sendMailError) {
+          logger.error(sendMailError);
+        }
       }
+      transporter.close();
     } else {
       transporter.close();
     }
@@ -164,42 +151,42 @@ export const sendReports: () => Promise<void> = async () => {
 /**
  * @deprecated in favour of noddemailer
  */
-export const sendReportsMailgun: () => Promise<void> = async () => {
-  try {
-    const adminText = `
-## Reports:
-    ${JSON.stringify(db.getReports(), null, 2)}
-`;
-    const adminHtml = `${JSON.stringify(db.getReports(), null, 2)}`;
-    const adminForm = setupForm({
-      to: MAILGUN_TO!,
-      from: `Mailgun Sandbox <${MAILGUN_FROM}>`,
-      text: adminText,
-      html: adminHtml,
-      subject: `[Flusshygiene] Cronbot report`,
-      addFile: true,
-    });
+// export const sendReportsMailgun: () => Promise<void> = async () => {
+//   try {
+//     const adminText = `
+// ## Reports:
+//     ${JSON.stringify(db.getReports(), null, 2)}
+// `;
+//     const adminHtml = `${JSON.stringify(db.getReports(), null, 2)}`;
+//     const adminForm = setupForm({
+//       to: MAILGUN_TO!,
+//       from: `Mailgun Sandbox <${MAILGUN_FROM}>`,
+//       text: adminText,
+//       html: adminHtml,
+//       subject: `[Flusshygiene] Cronbot report`,
+//       addFile: true,
+//     });
 
-    try {
-      await got.post({
-        url: `https://api.mailgun.net/v3/${process.env.MAILGUN_DOMAIN}/messages`,
-        username: "api",
-        password: process.env.MAILGUN_APIKEY!,
-        body: adminForm,
-      });
-    } catch (eadmin) {
-      logger.error(JSON.stringify(eadmin));
-    }
-  } catch (error) {
-    throw error;
-  }
+//     try {
+//       await got.post({
+//         url: `https://api.mailgun.net/v3/${process.env.MAILGUN_DOMAIN}/messages`,
+//         username: "api",
+//         password: process.env.MAILGUN_APIKEY!,
+//         body: adminForm,
+//       });
+//     } catch (eadmin) {
+//       logger.error(JSON.stringify(eadmin));
+//     }
+//   } catch (error) {
+//     throw error;
+//   }
 
-  if (globals.ADMIN_ONLY === false) {
-    // send reports to all users
-    const sortedReports = db.getReportsSorted();
-    logger.info(JSON.stringify(sortedReports));
-  }
-};
+//   if (globals.ADMIN_ONLY === false) {
+//     // send reports to all users
+//     const sortedReports = db.getReportsSorted();
+//     logger.info(JSON.stringify(sortedReports));
+//   }
+// };
 
 export const buildReport: (data: IBuildReport) => IReport = ({
   id,
