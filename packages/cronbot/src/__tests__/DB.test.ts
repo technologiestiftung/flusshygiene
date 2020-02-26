1459a9e49177dc46d6dfae78d32107568eb420a1
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-02-26 10:27:36 +0100

    test(remove Schema)

--- packages/cronbot/src/__tests__/DB.test.ts
+++ packages/cronbot/src/__tests__/DB.test.ts
@@ -1,6 +1,6 @@
 import { IObject, IReport } from "./../../dist/common/interfaces.d";
 import { IEndpoints, IGeneric } from "./../common/interfaces";
-import { DB, Schema } from "../lib/DB";
+import { DB } from "../lib/DB";
 
 const buildTestReport: (overrides?: IObject) => IReport = (overrides) => {
   const report: IReport = {
@@ -32,148 +32,181 @@ describe("DB", () => {
   });
 
   test("db type", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      expect(db).toBeInstanceOf(module.DB);
-    });
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        expect(db).toBeInstanceOf(module.DB);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
 
   test("getReportsSorted", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      db.addReports(buildTestReport({ type: "dataget", email: "foo@bah.com" }));
-      db.addReports(buildTestReport({ type: "dataget", email: "foo@bah.de" }));
-      db.addReports(buildTestReport({ type: "dataget", email: "foo@bah.xyz" }));
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        db.addReports(
+          buildTestReport({ type: "dataget", email: "foo@bah.com" }),
+        );
+        db.addReports(
+          buildTestReport({ type: "dataget", email: "foo@bah.de" }),
+        );
+        db.addReports(
+          buildTestReport({ type: "dataget", email: "foo@bah.xyz" }),
+        );
 
-      const reports = db.getReportsSorted();
-      expect(reports.length).toBe(3);
-    });
-  });
-
-  test("getReports/addReports", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      db.addReports({
-        id: "foo",
-        email: "foo@bah.com",
-        type: "admin",
-        message: "foo",
-        source: {} as IGeneric,
-        stack: "",
-        specifics: "",
+        const reports = db.getReportsSorted();
+        expect(reports.length).toBe(3);
+      })
+      .catch((err) => {
+        console.error(err);
       });
-      // console.log(db.getReports());
-      expect(db.getReports().length).toBe(1);
-    });
   });
 
   test("getReports/addReports", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      db.addReports([buildTestReport(), buildTestReport()]);
-      expect(db.getReports().length).toBe(2);
-    });
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        db.addReports({
+          id: "foo",
+          email: "foo@bah.com",
+          type: "admin",
+          message: "foo",
+          source: {} as IGeneric,
+          stack: "",
+          specifics: "",
+        });
+        // console.log(db.getReports());
+        expect(db.getReports().length).toBe(1);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
+  test("getReports/addReports", () => {
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        db.addReports([buildTestReport(), buildTestReport()]);
+        expect(db.getReports().length).toBe(2);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
   test("getEndpoints/addEndpoints", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      expect(db.getEndpoints).toBeDefined();
-      expect(db.getEndpoints()).toStrictEqual<IEndpoints[]>([]);
-      const data: IEndpoints[] = [
-        {
-          id: "foo",
-          user: { id: 1, email: "foo@bah.com" },
-          spot: { id: 1, name: "foo" },
-          discharges: [],
-          measurements: [],
-          globalIrradiances: [],
-        },
-      ];
-      db.addEndpoints(data);
-      expect(db.getEndpoints()[0]).toStrictEqual(data[0]);
-    });
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        expect(db.getEndpoints).toBeDefined();
+        expect(db.getEndpoints()).toStrictEqual<IEndpoints[]>([]);
+        const data: IEndpoints[] = [
+          {
+            id: "foo",
+            user: { id: 1, email: "foo@bah.com" },
+            spot: { id: 1, name: "foo" },
+            discharges: [],
+            measurements: [],
+            globalIrradiances: [],
+          },
+        ];
+        db.addEndpoints(data);
+        expect(db.getEndpoints()[0]).toStrictEqual(data[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
   test("getEndpoints/addEndpoints", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      const data: IEndpoints[] = [
-        {
-          id: "foo",
-          user: { id: 1, email: "foo@bah.com" },
-          spot: { id: 1, name: "foo" },
-          discharges: [],
-          measurements: [],
-          globalIrradiances: [],
-        },
-      ];
-      db.addEndpoints(data[0]);
-      expect(db.getEndpoints()[0]).toStrictEqual(data[0]);
-    });
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        const data: IEndpoints[] = [
+          {
+            id: "foo",
+            user: { id: 1, email: "foo@bah.com" },
+            spot: { id: 1, name: "foo" },
+            discharges: [],
+            measurements: [],
+            globalIrradiances: [],
+          },
+        ];
+        db.addEndpoints(data[0]);
+        expect(db.getEndpoints()[0]).toStrictEqual(data[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
 
   test("getGenerics/addGenerics", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      expect(db.getGenerics("genericInputs")).toStrictEqual([]);
-      const data: IGeneric[] = [
-        {
-          id: "foo",
-          user: { id: 1, email: "foo@bah.com" },
-          spot: { id: 1, name: "foo" },
-          pgId: 1,
-          name: "foo",
-          type: "Generische Werte",
-          data: [{ date: "2020-10-10", value: 1 }],
-        },
-      ];
-      db.addGenerics("genericInputs", data);
-      expect(db.getGenerics("genericInputs")[0]).toStrictEqual(data[0]);
-      db.resetState();
-      // console.log(db.getState());
-      expect(db.getState()).toStrictEqual<Schema>({
-        endpoints: [],
-        genericInputs: [],
-        purificationPlants: [],
-        reports: [],
-        spots: [],
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        expect(db.getGenerics("genericInputs")).toStrictEqual([]);
+        const data: IGeneric[] = [
+          {
+            id: "foo",
+            user: { id: 1, email: "foo@bah.com" },
+            spot: { id: 1, name: "foo" },
+            pgId: 1,
+            name: "foo",
+            type: "Generische Werte",
+            data: [{ date: "2020-10-10", value: 1 }],
+          },
+        ];
+        db.addGenerics("genericInputs", data);
+        expect(db.getGenerics("genericInputs")[0]).toStrictEqual(data[0]);
+      })
+      .catch((err) => {
+        console.error(err);
       });
-    });
   });
   test("addGenerics", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      expect(db.getGenerics("genericInputs")).toStrictEqual([]);
-      const data: IGeneric[] = [
-        {
-          id: "foo",
-          user: { id: 1, email: "foo@bah.com" },
-          spot: { id: 1, name: "foo" },
-          pgId: 1,
-          name: "foo",
-          type: "Generische Werte",
-          data: [{ date: "2020-10-10", value: 1 }],
-        },
-      ];
-      db.addGenerics("genericInputs", data[0]);
-      expect(db.getGenerics("genericInputs")[0]).toStrictEqual(data[0]);
-    });
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        expect(db.getGenerics("genericInputs")).toStrictEqual([]);
+        const data: IGeneric[] = [
+          {
+            id: "foo",
+            user: { id: 1, email: "foo@bah.com" },
+            spot: { id: 1, name: "foo" },
+            pgId: 1,
+            name: "foo",
+            type: "Generische Werte",
+            data: [{ date: "2020-10-10", value: 1 }],
+          },
+        ];
+        db.addGenerics("genericInputs", data[0]);
+        expect(db.getGenerics("genericInputs")[0]).toStrictEqual(data[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
   test("updateSpot", () => {
-    import("../lib/DB").then((module) => {
-      const db = module.DB.getInstance();
-      const sourceSpot = {
-        spotId: 1,
-        userId: 1,
-        spotName: "foo",
-        email: "foo@bah.com",
-        apiEndpoints: {},
-      };
-      db.setSpots([sourceSpot]);
-      let spots = db.getSpots();
-      expect(spots).toEqual([sourceSpot]);
-      db.updateSpot(spots[0].spotId, true);
-      spots = db.getSpots();
-      expect(spots[0].hasModel).toBe(true);
-    });
+    import("../lib/DB")
+      .then((module) => {
+        const db = module.DB.getInstance();
+        const sourceSpot = {
+          spotId: 1,
+          userId: 1,
+          spotName: "foo",
+          email: "foo@bah.com",
+          apiEndpoints: {},
+        };
+        db.setSpots([sourceSpot]);
+        let spots = db.getSpots();
+        expect(spots).toEqual([sourceSpot]);
+        db.updateSpot(spots[0].spotId, true);
+        spots = db.getSpots();
+        expect(spots[0].hasModel).toBe(true);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
 });
