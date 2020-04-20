fa20257e7b9d1ede21bc1f09716ac271b7686eda
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-04-20 09:20:31 +0200

    load balancer for new domain

--- terraform/eb-load-balancer/lb.tf
+++ terraform/eb-load-balancer/lb.tf
@@ -1,14 +1,14 @@
 data "aws_lb" "default" {
-  arn  = "${var.lb_arn}"
+  arn = var.lb_arn
 }
 
 data "aws_lb_listener" "default80" {
-  load_balancer_arn = "${data.aws_lb.default.arn}"
-  port = 80
+  load_balancer_arn = data.aws_lb.default.arn
+  port              = 80
 }
 
 resource "aws_lb_listener_rule" "redirect_http_to_https" {
-  listener_arn = "${data.aws_lb_listener.default80.arn}"
+  listener_arn = data.aws_lb_listener.default80.arn
 
   action {
     type = "redirect"
@@ -20,12 +20,12 @@ resource "aws_lb_listener_rule" "redirect_http_to_https" {
     }
   }
 
-# SEE for multiple values https://github.com/terraform-providers/terraform-provider-aws/issues/8266
+  # SEE for multiple values https://github.com/terraform-providers/terraform-provider-aws/issues/8266
   condition {
-    field  = "host-header"
+    field = "host-header"
+
     # values = ["*.flusshygiene.xyz","www.flusshygiene.xyz","flusshygiene.xyz"]
-    values = ["*.flusshygiene.xyz"]
+    values = ["*.flussbaden.org"]
   }
 }
-
 
