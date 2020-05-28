01286902f49047bc0df5c930f86727e3c32c25d0
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-28 16:49:42 +0200

    renovate auth0 lib

--- packages/cms-spa/src/__tests__/App.test.tsx
+++ packages/cms-spa/src/__tests__/App.test.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-// import App from '../App';
+// import App from "../App";
+
 // import { createStore } from 'redux';
 // import { reducer } from '../../__test-utils/empty-reducer';
 // import { initialState } from '../../__test-utils/initial-state';
-// import { render, act } from '../../__test-utils/render-with-providers';
-// import { createMemoryHistory } from 'history';
+// import { render } from "../../__test-utils/render-with-providers";
+// import { createMemoryHistory } from "history";
+// jest.mock("@react-pdf/renderer");
 
-it.skip("renders App without crashing", () => {
+test.skip("renders App without crashing", () => {
   // const history = createMemoryHistory();
-  // const store = createStore(reducer, initialState);
-  // const div = document.createElement('div');
-  // act(() => {
-  // const { container } = render(<App />, store, history);
-  // });
+  // // const store = createStore(reducer, initialState);
+  // const div = document.createElement("div");
+  // // act(() => {
+  // const { container } = render(<App />, undefined, history);
+  // // });
   // expect(container).toBeTruthy();
-  // ReactDOM.render(
-  //   <Provider store={store}>
-  //     <App />
-  //   </Provider>,
+  // render(
+  //   //   <Provider store={store}>
+  //   <App />,
+  //   //   </Provider>,
   //   div,
   // );
-  // ReactDOM.unmountComponentAtNode(div);
 });
--- packages/cms-spa/src/__tests__/CardTile.test.tsx
+++ packages/cms-spa/src/__tests__/CardTile.test.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { reducer } from "../../__test-utils/empty-reducer";
-import { initialState } from "../../__test-utils/initial-state";
 import { render } from "../../__test-utils/render-with-providers";
 import { createMemoryHistory } from "history";
 import { CardTile } from "../components/spot/elements/Spot-CardTile";
