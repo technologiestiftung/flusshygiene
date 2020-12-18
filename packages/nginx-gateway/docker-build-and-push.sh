bb738a7999795a36d52ec10429186f51e93b4cdb
ff6347 <fabianmoronzirfas@protonmail.ch>
2020-12-18 15:26:27 +0100

    fix leaked pw

--- packages/nginx-gateway/docker-build-and-push.sh
+++ packages/nginx-gateway/docker-build-and-push.sh
@@ -6,7 +6,6 @@ GITHUB_REPOSITORY="technologiestiftung/flusshygiene"
 GITHUB_REF="test"
 SUFFIX=${PWD##*/}
 STAGE="dev"
-OCPU_PW="test"
 OCPU_USER="test"
 # PUSHIT=FALSE
 
@@ -14,24 +13,23 @@ print_usage() {
   printf "\n\nUsage:------------------------------\n"
   printf "Usage: %s -t yourtag -s stage\n" "${0}"
   printf "       If -u flag is not specified it will use '%s'\n" $OCPU_USER
-  printf "       If -p flag is not specified it will use '%s'\n" $OCPU_PW
   printf "       If -t flag is not specified it will use '%s'\n" $GITHUB_REF
   printf "       If -s flag is not specified it will use '%s'\n\n\n" $STAGE
-    # printf "       If -r flag is not specified it will not push to the remote '%s'\n\n\n" $PUSHIT
+  # printf "       If -r flag is not specified it will not push to the remote '%s'\n\n\n" $PUSHIT
 
 }
 
-while getopts 't:s:p:u:' flag; do
+while getopts 't:s:u:' flag; do
   case "${flag}" in
-    t) GITHUB_REF="${OPTARG}" ;;
-    s) STAGE="${OPTARG}" ;;
-    p) OCPU_PW="${OPTARG}" ;;
-    u) OCPU_USER="${OPTARG}" ;;
-    *) print_usage
-       exit 1 ;;
+  t) GITHUB_REF="${OPTARG}" ;;
+  s) STAGE="${OPTARG}" ;;
+  u) OCPU_USER="${OPTARG}" ;;
+  *)
+    print_usage
+    exit 1
+    ;;
   esac
 done
-
 
 echo "${GITHUB_REPOSITORY}"
 echo "${GITHUB_REF}"
@@ -42,15 +40,13 @@ echo "${OCPU_USER}"
 echo "Your image will be build with this repository/tag: '${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}'"
 echo "Your user for the ocpu/test will be ${OCPU_USER}. Your pw will not be shown here"
 read -p "Are you sure?(y/n) " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+echo # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   echo "abort!"
   print_usage
   exit 1
 fi
 
-
- docker build --build-arg OCPU_USER="${OCPU_USER}" --build-arg OCPU_PW="${OCPU_PW}" --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-dev" .
+docker build --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-dev" .
 
 docker push "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-dev"
--- packages/opencpu-fhpredict-api/docker-build-and-push.sh
+++ packages/opencpu-fhpredict-api/docker-build-and-push.sh
@@ -5,7 +5,6 @@ IFS=$'\n\t'
 export $(grep -E -v '^#' .env | xargs -0)
 GITHUB_REPOSITORY="technologiestiftung/flusshygiene"
 GITHUB_REF="test"
-OCPU_PW="opencpu"
 SUFFIX=${PWD##*/}
 STAGE="dev"
 
@@ -14,14 +13,12 @@ print_usage() {
   printf "Usage: %s -t yourtag -s stage\n" "${0}"
   printf "       If -t flag is not specified it will use '%s'\n" $GITHUB_REF
   printf "       If -s flag is not specified it will use '%s'\n\n\n" $STAGE
-  printf "       If -p flag is not specified it will use '%s'\n\n\n" $OCPU_PW
 }
 
-while getopts 't:s:p:' flag; do
+while getopts 't:s:' flag; do
   case "${flag}" in
   t) GITHUB_REF="${OPTARG}" ;;
   s) STAGE="${OPTARG}" ;;
-  p) OCPU_PW="${OPTARG}" ;;
   *)
     print_usage
     exit 1
@@ -34,7 +31,6 @@ echo "${GITHUB_REF}"
 echo "${SUFFIX}"
 echo "${STAGE}"
 echo "${GITHUB_PAT}"
-echo "${OCPU_PW}"
 
 echo "Your image will be build with this repository/tag: '${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}'"
 read -p "Are you sure?(y/n) " -n 1 -r
@@ -44,6 +40,6 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   print_usage
   exit 1
 fi
-docker build --build-arg GITHUB_PAT="${GITHUB_PAT}" --build-arg OCPU_PW="${OCPU_PW}" --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}" .
+docker build --build-arg GITHUB_PAT="${GITHUB_PAT}" --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}" .
 # docker build  --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}" .
 docker push "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}"
