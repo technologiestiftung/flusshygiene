4d29a443f1cfb23075404bb249209fe47971748f
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-01-20 14:23:22 +0100

    fix(leak): remove usage of github_pat

--- packages/opencpu-fhpredict-api/docker-build-and-push.sh
+++ packages/opencpu-fhpredict-api/docker-build-and-push.sh
@@ -30,7 +30,6 @@ echo "${GITHUB_REPOSITORY}"
 echo "${GITHUB_REF}"
 echo "${SUFFIX}"
 echo "${STAGE}"
-echo "${GITHUB_PAT}"
 
 echo "Your image will be build with this repository/tag: '${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}'"
 read -p "Are you sure?(y/n) " -n 1 -r
@@ -40,6 +39,6 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   print_usage
   exit 1
 fi
-docker build --build-arg GITHUB_PAT="${GITHUB_PAT}" --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}" .
+docker build --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}" .
 # docker build  --tag "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}" .
 docker push "${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}"
