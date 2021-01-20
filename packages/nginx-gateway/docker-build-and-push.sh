1e0d43b545fffbf6eac701e7e7a8d6083e27db90
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-01-20 14:34:41 +0100

    remove ocpu user env variable

--- packages/nginx-gateway/docker-build-and-push.sh
+++ packages/nginx-gateway/docker-build-and-push.sh
@@ -6,24 +6,21 @@ GITHUB_REPOSITORY="technologiestiftung/flusshygiene"
 GITHUB_REF="test"
 SUFFIX=${PWD##*/}
 STAGE="dev"
-OCPU_USER="test"
 # PUSHIT=FALSE
 
 print_usage() {
   printf "\n\nUsage:------------------------------\n"
   printf "Usage: %s -t yourtag -s stage\n" "${0}"
-  printf "       If -u flag is not specified it will use '%s'\n" $OCPU_USER
   printf "       If -t flag is not specified it will use '%s'\n" $GITHUB_REF
   printf "       If -s flag is not specified it will use '%s'\n\n\n" $STAGE
   # printf "       If -r flag is not specified it will not push to the remote '%s'\n\n\n" $PUSHIT
 
 }
 
-while getopts 't:s:u:' flag; do
+while getopts 't:s:' flag; do
   case "${flag}" in
   t) GITHUB_REF="${OPTARG}" ;;
   s) STAGE="${OPTARG}" ;;
-  u) OCPU_USER="${OPTARG}" ;;
   *)
     print_usage
     exit 1
@@ -35,10 +32,9 @@ echo "${GITHUB_REPOSITORY}"
 echo "${GITHUB_REF}"
 echo "${SUFFIX}"
 echo "${STAGE}"
-echo "${OCPU_USER}"
 
 echo "Your image will be build with this repository/tag: '${GITHUB_REPOSITORY}-${SUFFIX}:${GITHUB_REF}-${STAGE}'"
-echo "Your user for the ocpu/test will be ${OCPU_USER}. Your pw will not be shown here"
+echo "Your user for the ocpu/test will be 'opencpu'. Your pw will not be shown here"
 read -p "Are you sure?(y/n) " -n 1 -r
 echo # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
